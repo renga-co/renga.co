b5726cba5e134192e56fcf19eadddbf800b69ac6
Ross Zurowski <ross@rosszurowski.com>
2018-09-19 21:45:41 -0400

    Update footer navigation for mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,30 +1,29 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import cx from 'classnames';
 import Link from 'gatsby-link';
 
-const Divider = () => (
-  <span className="p-relative ph-2 c-gray2" style={{ fontSize: 10, top: -2 }}>
+const Divider = ({ className, ...props }) => (
+  <span
+    className={cx('p-relative ph-2 c-gray2', className)}
+    style={{ fontSize: 10, top: -2 }}>
     &bull;
   </span>
 );
-
-const links = [
-  { href: '/about', label: 'About' },
-  { href: '/services', label: 'Services' },
-  { href: '/blog', label: 'Blog' },
-  { href: '/careers', label: 'Careers' },
-  { href: 'mailto:hello@renga.co', label: 'Contact Us' },
-];
 
 const Footer = () => (
   <footer
     className="fs-16 pv-6 ph-3 c-gray3 ta-center lh-1d7"
     style={{ marginTop: 'auto' }}>
-    {links.map((link, i) => (
-      <Fragment key={link.href}>
-        <Link to={link.href}>{link.label}</Link>
-        {i < links.length - 1 && <Divider />}
-      </Fragment>
-    ))}
+    <Link to="/about">About</Link>
+    <Divider />
+    <Link to="/services">Services</Link>
+    <Divider />
+    <Link to="/blog">Blog</Link>
+    <br className="d-none-m" />
+    <Divider className="d-none d-inlineBlock-m" />
+    <Link to="/careers">Careers</Link>
+    <Divider />
+    <Link to="mailto:hello@renga.co">Contact Us</Link>
   </footer>
 );
 
