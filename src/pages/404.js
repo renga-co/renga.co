b9ac6794d8c81a2014fc3182a27f9671752a6c56
Ross Zurowski <ross@rosszurowski.com>
2018-09-19 22:11:05 -0400

    Use content component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Content from '../components/content';
 
 const NotFoundPage = () => (
   <Layout>
     <div className="mw-700 mh-auto mt-5 ta-center">
-      <div className="PostContent">
+      <Content>
+        <p>Sorry, this page doesn't seem to exist.</p>
         <p>
-          Sorry, this page doesn't seem to exist. <a href="/">Return home?</a>
+          <a href="/">&larr; Return home</a>
         </p>
-      </div>
+      </Content>
     </div>
   </Layout>
 );
--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Header from '../components/header';
+import Content from '../components/content';
 
 const CareersPage = () => (
   <Layout>
     <div className="mw-700 ta-center mh-auto">
-      <div className="PostContent">
-        <Header
-          title="Work with us"
-          subtitle={`We’re always looking for talented designers, developers, writers and
-      media creators. We’d love to see your portfolio and chat.`}
-        />
+      <Header
+        title="Work with us"
+        subtitle={`We’re always looking for talented designers, developers, writers and
+    media creators. We’d love to see your portfolio and chat.`}
+      />
+      <Content>
         <p>
           Get in touch at{' '}
-          <a
-            className="d-inlineBlock c-geraldine"
-            href="mailto:careers@renga.co">
-            careers@renga.co &rarr;
-          </a>
+          <a href="mailto:careers@renga.co">careers@renga.co &rarr;</a>
         </p>
-      </div>
+      </Content>
     </div>
   </Layout>
 );
